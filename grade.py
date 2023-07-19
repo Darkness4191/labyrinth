f7ba845273984b1c2d13373701aa662bc9756aa6
Dragon777 <chris.igel@outlook.de>
2023-07-19 11:58:00 +0200

    add grade.py

--- grade.py
+++ grade.py
@@ -5,18 +5,43 @@ MEDIUM = 1
 HARD = 2
 VERY_HARD = 3
 
+HARDEST_LENGTH_PERCENT = 0.6
+
+# 1: Gesamt Länge (abs(--)) * Abzweigungen (n: Länge / Länge zum Startpunkt)
+# 2: Abzweigungen
+#    - Länge / Länge zum Startpunkt
+
 class Search:
     def __init__(self, difficulty):
         self.difficulty
+
+    def _shortest_path_to_start(self, point):
 
     def search_for_maze(self, width, height, n=50):
         self.labyrinth = Labyrinth(width, height)
 
         best = self.labyrinth.generate_maze()
+        best_diff = self.grade(best)
 
         for i in range(n):
-            pass
+            self.labyrinth.clear_maze()
 
+            new_laby = self.labyrinth.generate_maze()
+            new_diff = self.grade(new_laby)
 
-    def grade(self, labyrinth):
-        pass
+            if abs(new_diff - self.difficulty) < abs(best_diff - self.difficulty):
+                best = new_laby
+                best_diff = new_diff
+
+    def grade(self, labyrinth : Labyrinth):
+        whole_length = 0# last -> parent -> parent -> ... -> start (n)
+
+        min_length = labyrinth.height + (labyrinth.width - 1)
+        max_length = labyrinth.width * labyrinth.height
+        hardest_length = HARDEST_LENGTH_PERCENT * max_length
+
+        length_grade = abs(hardest_length - whole_length)
+
+        junction_count = 0# loop tree -> search for more than one children
+
+        junction_grade = 0 #[] ++ loop junctions -> length / length to start
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 
 import game, laby, threading, pickle
 
-l = laby.Labyrinth(12, 12)
+l = laby.Labyrinth(10, 10)
 
 def gen_maze(name):
     time.sleep(1)
@@ -10,21 +10,21 @@ def gen_maze(name):
 
 if __name__ == '__main__':
     #l.remove_wall(l.matrix[0][0], l.matrix[1][0])
-    #threading.Thread(target=gen_maze, args=(1,)).start()
-    l.generate_maze()
+    threading.Thread(target=gen_maze, args=(1,)).start()
+    #l.generate_maze()
 
     with open("matrix.json", "wb") as f:
         pickle.dump(l, f)
 
-    # Get the descendants of the node
-    descendants = l.tree.children(f"{l.width - 1},{l.height - 1}")
+    # # Get the descendants of the node
+    # descendants = l.tree.children(f"{l.width - 1},{l.height - 1}")
+    #
+    # # Delete the descendants
+    # for descendant in descendants:
+    #     l.tree.remove_subtree(descendant.identifier)
 
-    # Delete the descendants
-    for descendant in descendants:
-        l.tree.remove_subtree(descendant.identifier)
-
-    with open("tree.txt", "w") as f:
-        f.write(l.tree.show(stdout=False))
+    # with open("tree.txt", "w") as f:
+    #     f.write(l.tree.show(stdout=False))
 
     g = game.Game(l)
     g.show()
